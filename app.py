dddfa46d677c7921e051e35c786a13d0425411cf
moussamarmouch <mokromike@gmail.com>
2022-02-20 02:18:13 +0100

    check mac adress for vendor and show on site & added materialize css and templates

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import subprocess
-
-from flask import Flask
+import csv
+from flask import Flask,render_template
+from mac_vendor_lookup import MacLookup
 app = Flask(__name__)
+
+# MacLookup().update_vendors()
 
 @app.route("/")
 def hello():
@@ -9,8 +12,19 @@ def hello():
 
 @app.route("/12")
 def titel2():
-    subprocess.Popen("fing -r 1 -o log,csv,test.csv", shell=True, stdout=subprocess.PIPE).stdout.read()
-    return "test"
+    mac = []
+    subprocess.Popen("fing -r 1 -o log,csv,data/test.csv", shell=True, stdout=subprocess.PIPE).stdout.read()
+    with open('data/test.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        for row in reader:
+            try:
+                vendor = MacLookup().lookup(row[5])
+            except KeyError:
+                vendor = " "
+            yes = (row[5], vendor)
+            mac.append(yes)
+        csvfile.close
+    return render_template('index.html',header='House', mac_list = mac)               
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
