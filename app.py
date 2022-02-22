9290a6c2bd3d2ba9dcef64d091744b4dbef9291d
moussamarmouch <mokromike@gmail.com>
2022-02-22 15:07:36 +0100

    compare scan with known and display

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import subprocess
 import csv
 from flask import Flask,render_template
+from flask_debugtoolbar import DebugToolbarExtension
+import logging
 from mac_vendor_lookup import MacLookup
-app = Flask(__name__)
 
-MacLookup().update_vendors()
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
@@ -13,20 +14,19 @@ def hello():
 @app.route("/12")
 def titel2():
     mac = []
+    known = []
     subprocess.Popen("fing -r 1 -o log,csv,data/test.csv", shell=True, stdout=subprocess.PIPE).stdout.read()
-    with open('data/test.csv', newline='') as csvfile:
+    with open('data/test.csv', newline='') as csvfile, open('data/known.csv', newline='') as csvfile1:
         reader = csv.reader(csvfile, delimiter=';')
+        reader1 = csv.reader(csvfile1, delimiter=';')
         for row in reader:
-            try:
-                vendor = MacLookup().lookup(row[5])
-            except KeyError:
-                vendor = " "
-
-            yes = (row[5], vendor)
-            mac.append(yes)
+            mac.append(row[5])
         csvfile.close
-
-    return render_template('index.html',header='House', mac_list = mac)               
+        for know in reader1: 
+            if know[0] in mac:
+                known.append(know[1])
+    csvfile1.close
+    return render_template('index.html',header='House', mac_list = known)               
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
